94df6d4b9042490d57e3984dba74bfecdb88f4ab
Valentin Chudinov <valentinchudinov@gmail.com>
2023-02-21 00:48:02 +0700

    last commit

---
+++ Task002_twoPoints/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.WriteLine("Enter point A coordinates (x, y, z): ");
+double x1 = double.Parse(Console.ReadLine());
+double y1 = double.Parse(Console.ReadLine());
+double z1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter point B coordinates (x, y, z): ");
+double x2 = double.Parse(Console.ReadLine());
+double y2 = double.Parse(Console.ReadLine());
+double z2 = double.Parse(Console.ReadLine());
+
+double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+
+Console.WriteLine("Distance between point A and point B: " + distance);
---
+++ Task003_squareTable/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Enter a number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Number\tCube");
+for (int i = 1; i <= n; i++)
+{
+    int cube = Convert.ToInt32(Math.Pow(i, 3));
+    Console.WriteLine("{0}\t{1}", i, cube);
+}
